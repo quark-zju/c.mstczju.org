06356544479855e618b52ecbba3cecd178ff77db
WU Jun <quark@lihdd.net>
2010-12-09 23:26:20 +0800

    fix a typo

--- other_config/judge_starter/judge_starter.rb
+++ other_config/judge_starter/judge_starter.rb
@@ -48,38 +48,40 @@ threads = {}
 # single thread, feels better
 sleep_time = 0.1
 while true do
-	# find not judged in transaction
-	j = nil
+	# find not judged, update it as judging in transaction
+	s = nil
+	sleep sleep_time
+
 	Submission.transaction do
 		s = Submission.find(:first,
 							:order => 'created_at ASC', 
 							:conditions => "result = 0")
-		if j.nil?
-			sleep_time += 0.5
-			sleep_time = 5 if sleep_time > 5
+		if s.nil?
+			sleep_time += 0.1
+			sleep_time = 3.5 if sleep_time > 3.5
 		else
 			sleep_time = 0.0
-			break
+			s.update_attributes!(:result => 1)
+			break # skip sleep to begin judge that submission
 		end
-		sleep sleep_time
-		s.update_attributes!(:result => 1)
 	end
 
-	puts 'Begin judge ...'
+	next if s.nil?
+
 	# not_judged.each do |s|
-		j = JUDGE_GROUP[JUDGE_NO]
+	j = JUDGE_GROUP[JUDGE_NO]
 
-		# run judge
-		output = `ssh #{j} '~/run.rb' '#{s.lang}' '#{submission_path(s.id)}' '#{problem_path(s.problem_id)}' '#{s.problem.time_limit}' '#{s.problem.memory_limit}'`
+	# run judge
+	output = `ssh #{j} '~/run.rb' '#{s.lang}' '#{submission_path(s.id)}' '#{problem_path(s.problem_id)}' '#{s.problem.time_limit}' '#{s.problem.memory_limit}'`
 
-		# get result
-		stat, time, memory = 11, nil, nil # 11: internal error
-		if output =~ /^Result = (?<stat>.*)$/
-			stat = $~[:stat].to_i
-		end
+	# get result
+	stat, time, memory = 11, nil, nil # 11: internal error
+	if output =~ /^Result = (?<stat>.*)$/
+		stat = $~[:stat].to_i
+	end
 
-		# get memory and time if exist
-		time = $~[:time] if output =~ /^Time = (?<time>.*)$/
+	# get memory and time if exist
+	time = $~[:time] if output =~ /^Time = (?<time>.*)$/
 		memory = $~[:memory] if output =~ /^Memory = (?<memory>.*)$/
 
 		if stat == 2
@@ -92,28 +94,28 @@ while true do
 			end
 		end
 
-		# do not update anything when Internal Error
-		if stat != 11
-			# update submit count
-			accepted = (stat == 3)
-			submit_count, accept_count = s.problem.submit_count, s.problem.accept_count
+	# do not update anything when Internal Error
+	if stat != 11
+		# update submit count
+		accepted = (stat == 3)
+		submit_count, accept_count = s.problem.submit_count, s.problem.accept_count
 
-			accept_count += 1 if accepted
-			submit_count += 1
+		accept_count += 1 if accepted
+		submit_count += 1
 
-			# update user_cache
-			user_cache = UserCache.find(:first, :conditions => [ "user_id = :u and problem_id = :p", { :u => s.user_id, :p => s.problem_id} ]) || UserCache.new(:user_id => s.user_id, :problem_id => s.problem_id)
+		# update user_cache
+		user_cache = UserCache.find(:first, :conditions => [ "user_id = :u and problem_id = :p", { :u => s.user_id, :p => s.problem_id} ]) || UserCache.new(:user_id => s.user_id, :problem_id => s.problem_id)
 
-			if accepted
-				user_cache.first_accepted_time ||= s.created_at
-			elsif user_cache.first_accepted_time.nil?
-				user_cache.attempt = (user_cache.attempt or 0) + 1
-			end
-			user_cache.save
+		if accepted
+			user_cache.first_accepted_time ||= s.created_at
+		elsif user_cache.first_accepted_time.nil?
+			user_cache.attempt = (user_cache.attempt or 0) + 1
 		end
+		user_cache.save
+	end
 
-		s.problem.update_attributes!(:submit_count => submit_count, :accept_count => accept_count)
-		s.update_attributes!(:result => stat, :used_time => time, :used_memory => memory)
+	s.problem.update_attributes!(:submit_count => submit_count, :accept_count => accept_count)
+	s.update_attributes!(:result => stat, :used_time => time, :used_memory => memory)
 	# end
 end
 
