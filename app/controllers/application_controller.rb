4abf36c6496cd8d65d26d044b39c2760e79d2849
WU Jun <quark@lihdd.net>
2010-12-10 19:39:11 +0800

    friendly error page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,36 @@ class ApplicationController < ActionController::Base
 
   include SessionsHelper
 
+
+  #ActiveRecord exceptions
+  rescue_from ActiveRecord::RecordNotFound, :with => :not_found #400   
+
+  #ActiveResource exceptions  
+  #rescue_from ActiveResource::ResourceNotFound, :with => :not_found #404
+
+  #ActionView exceptions
+  rescue_from ActionView::TemplateError, :with => :not_found #500
+
+  #ActionController exceptions
+  rescue_from ActionController::RoutingError, :with => :not_found #404   
+
+  rescue_from ActionController::UnknownController, :with => :not_found #404 
+
+  rescue_from ActionController::MethodNotAllowed, :with => :not_found #405   
+
+  rescue_from ActionController::InvalidAuthenticityToken, :with => :not_found #405
+
+  rescue_from ActionController::UnknownAction, :with => :not_found #501
+
+  # This particular exception causes all the rest to fail.... why?
+  # rescue_from ActionController::MissingTemplate, :with => :not_found #404
+
+  protected
+
+  def not_found
+    render :text => "Error", :status => 404
+  end
+
+
   private
 end
